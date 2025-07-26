533401bd4355c6f2a687bf5012a7edcf2a8b7f40
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-26 17:29:45 +0100

    Created basic user greeting script

---
+++ Scripts/greet-user.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# This script is inspired by an example from the book Linux Basics for Hackers
+# Please ignore it's passive aggressiveness
+
+echo "Hi! What is your name?"
+
+read name
+
+echo "And how old are you?"
+
+read age
+
+echo "Welcome" $name"!. You're" $age "? That's kinda old!"
